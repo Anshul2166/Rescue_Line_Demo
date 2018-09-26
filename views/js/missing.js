8448f7e1a67c001861c923f8bc937498fb74da33
Anshul Dubey <anshuldubey2166@gmail.com>
2018-09-27 03:13:00 +0530

    Able to set location in missing

--- views/js/missing.js
+++ views/js/missing.js
@@ -21,9 +21,10 @@ $(document).on("ready", function() {
 		console.log("Preventing");
 		e.preventDefault();
 		console.log("Sending in formData");
-		for(var pair of formData.entries()) {
-     		console.log(pair[0]+ ', '+ pair[1]); 
+		for (var pair of formData.entries()) {
+			console.log(pair[0] + ", " + pair[1]);
 		}
+		locationMarking();
 		// console.log(formData);
 		// $.ajax({
 		// 	method: "POST",
@@ -43,9 +44,73 @@ $(document).on("ready", function() {
 	});
 });
 function readURL(input) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$("#missing_img").attr("src", e.target.result);
-		};
-		reader.readAsDataURL(input.files[0]);
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		$("#missing_img").attr("src", e.target.result);
+	};
+	reader.readAsDataURL(input.files[0]);
+}
+
+function locationMarking() {
+	var self=this;
+	self.loaded = false;
+  	self.map = null;
+	console.log("More inside");
+	swal({
+      title: "Your Location Is Critical",
+      text: "Send us the location of last seen",
+      icon: "info"
+    }).then(function(){
+      self.load();
+      $('#location_prompt').show();
+      // localStorage.setItem('location-prompt',new Date().getTime());
+    });
+	this.load = function(){
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoicm9naTU1NSIsImEiOiJjajh1MjJnYTYwdXU4MzNtYnZ5NHl1dGhpIn0.CBUJUe_M8sLWykZgHIpfIw';
+    self.map = new mapboxgl.Map({
+      container: 'prompt_map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-74.50, 40], // starting position [lng, lat]
+      zoom: 13 // starting zoom
+    });
+
+    var nav = new mapboxgl.NavigationControl({ showCompass : false });
+    var geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: false,
+        showUserLocation: false
+    });
+
+    var markerCreated = false;
+
+    geolocate.on('geolocate', function(e) {
+
+      if (markerCreated)
+        return false;
+
+      var marker = new mapboxgl.Marker({ color: "#c74b3b" })
+        .setDraggable(true)
+        .setLngLat({ lat: e.coords.latitude , lng: e.coords.longitude })
+        .addTo(self.map);
+
+      markerCreated = true;
+
+      localStorage.setItem('location-precise',JSON.stringify({ lat: e.coords.latitude , lng: e.coords.longitude }));
+
+      marker.on('dragend',function(e){
+        console.log(e);
+        localStorage.setItem('location-precise',JSON.stringify(e.target._lngLat));
+      });
+
+    });
+
+    self.map.addControl(nav, 'top-left');
+    self.map.addControl(geolocate);
+
+    self.loaded = true;
+
+  };
 }
