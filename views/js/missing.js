488fbd06f49a4668e6aacf3925816d9c57da9e00
Anshul Dubey <anshuldubey2166@gmail.com>
2018-09-28 03:22:16 +0530

    Get and final count working

--- views/js/missing.js
+++ views/js/missing.js
@@ -1,5 +1,6 @@
 $(document).on("ready", function() {
 	//code to run at page load
+	var chatHandler = new ChatHandler();
 	console.log("Here in missing");
 	var formData;
 	$("#missing_pic").on("change", function() {
@@ -22,42 +23,135 @@ $(document).on("ready", function() {
 		e.preventDefault();
 		let location = JSON.parse(
 			localStorage.getItem("missing-location-precise")
-		);
+			);
 		let lat = location.lat;
 		let lng = location.lng;
-		console.log(location);
-		console.log(lat);
+		let first_name =
+		$('#missing_form input[name="first_name"]').val() || "";
+		let last_name = $('#missing_form input[name="last_name"]').val() || "";
 		formData.append("lat", lat);
 		formData.append("lon", lng);
+		formData.append("lon", lng);
+		formData.append("lon", lng);
+		formData.append("first_name", first_name);
+		formData.append("last_name", last_name);
 		console.log("Sending in formData");
 		for (var pair of formData.entries()) {
 			console.log(pair[0] + ", " + pair[1]);
 		}
 		handleSuccess("Added missing report. You will get notified on result");
 		$.ajax({
-			method: "POST",
-			url: "http://investorrank.in/api/missing/",
-			contentType: "application/json",
-			dataType: "jsonp",
-			// crossDomain: true,
-			processData: false,
-			contentType: false,
-			// headers: {
-			//            "Access-Control-Allow-Origin":"*"
-			//          },
-			// data: JSON.stringify({
-			// 	input: message,
-			// 	token: token,
-			// 	location: localStorage.getItem("location-precise")
-			// })
-			data: formData
-		}).done(function(response) {
-			console.log("Got the response from the server");
+			method: "GET",
+			url:
+			"https://cors-anywhere.herokuapp.com/investorrank.in/api/missing/"
+		})
+		.done(function(response) {
+			console.log("Sending in get-response-success");
 			console.log(response);
-		}).fail(function(error){
-			console.log("error here");
-			console.log(error);
+			let results = response.results;
+			let max = 0;
+			let final_result;
+			for (var i = 0; i < results.length; i++) {
+				if (max < results[i].id) {
+					max = results[i].id;
+					final_result = results[i];
+				}
+			}
+			console.log(final_result);
+			if (final_result.image_found == true) {
+				console.log("Found the image");
+				let first_name = final_result.first_name;
+				let last_name = final_result.last_name;
+				let name = "";
+				if (first_name == "" || last_name == "") {
+					name = first_name + last_name;
+				} else {
+					name = first_name + " " + last_name;
+				}
+				let lat = final_result.lat;
+				let lon = final_result.lon;
+				chatHandler.startChat(
+					"anshul2166",
+					Cookies.get("token")
+					);
+				let message =
+				"Found your missing person " +
+				name +
+				". The last position was latitude:" +
+				lat +
+				" and longitude:" +
+				lon;
+				chatHandler.sendChat(message, Cookies.get("token"));
+			}
+		})
+		.fail(function(err) {
+			console.log("Error in sending user profile1");
+			console.log(err);
 		});
+		// $.ajax({
+		// 	method: "POST",
+		// 	url:
+		// 	"https://cors-anywhere.herokuapp.com/investorrank.in/api/missing/",
+		// 	crossDomain: true, // tell browser to allow cross domain.
+		// 	processData: false,
+		// 	contentType: false,
+		// 	data: formData
+		// })
+		// .done(function(response) {
+		// 	console.log("Sending in response-success");
+		// 	$.ajax({
+		// 		method: "GET",
+		// 		url:
+		// 		"https://cors-anywhere.herokuapp.com/investorrank.in/api/missing/"
+		// 	})
+		// 	.done(function(response) {
+		// 		console.log("Sending in get-response-success");
+		// 		console.log(response);
+		// 		let data = response.results;
+		// 		let max = 0;
+		// 		let final_result;
+		// 		for (var i = 0; i < results.length; i++) {
+		// 			if (max < results[i].id) {
+		// 				max = results[i].id;
+		// 				final_result = results[i];
+		// 			}
+		// 		}
+		// 		console.log(final_result);
+		// 		if (final_result.image_found == true) {
+		// 			console.log("Found the image");
+		// 			let first_name = final_result.first_name;
+		// 			let last_name = final_result.last_name;
+		// 			let name = "";
+		// 			if (first_name == "" || last_name == "") {
+		// 				name = first_name + last_name;
+		// 			} else {
+		// 				name = first_name + " " + last_name;
+		// 			}
+		// 			let lat = final_result.lat;
+		// 			let lon = final_result.lon;
+		// 			chatHandler.startChat(
+		// 				"anshul2166",
+		// 				Cookies.getToken()
+		// 				);
+		// 			let message =
+		// 			"Found your missing person " +
+		// 			name +
+		// 			". The last position was latitude:" +
+		// 			lat +
+		// 			" and longitude:" +
+		// 			lon;
+		// 			chatHandler.sendChat(message, Cookies.get("token"));
+		// 		}
+		// 	})
+		// 	.fail(function(err) {
+		// 		console.log("Error in sending user profile1");
+		// 		console.log(err);
+		// 	});
+		// })
+		// .fail(function(err) {
+		// 	console.log("Error in sending user profile2");
+		// 	console.log(err);
+		// });
 	});
 });
 function readURL(input) {
@@ -90,7 +184,7 @@ function locationMarking() {
 	});
 	this.load = function() {
 		mapboxgl.accessToken =
-			"pk.eyJ1Ijoicm9naTU1NSIsImEiOiJjajh1MjJnYTYwdXU4MzNtYnZ5NHl1dGhpIn0.CBUJUe_M8sLWykZgHIpfIw";
+		"pk.eyJ1Ijoicm9naTU1NSIsImEiOiJjajh1MjJnYTYwdXU4MzNtYnZ5NHl1dGhpIn0.CBUJUe_M8sLWykZgHIpfIw";
 		self.map = new mapboxgl.Map({
 			container: "prompt_map",
 			style: "mapbox://styles/mapbox/streets-v10",
@@ -113,9 +207,9 @@ function locationMarking() {
 			if (markerCreated) return false;
 
 			var marker = new mapboxgl.Marker({ color: "#c74b3b" })
-				.setDraggable(true)
-				.setLngLat({ lat: e.coords.latitude, lng: e.coords.longitude })
-				.addTo(self.map);
+			.setDraggable(true)
+			.setLngLat({ lat: e.coords.latitude, lng: e.coords.longitude })
+			.addTo(self.map);
 
 			markerCreated = true;
 
@@ -125,14 +219,14 @@ function locationMarking() {
 					lat: e.coords.latitude,
 					lng: e.coords.longitude
 				})
-			);
+				);
 
 			marker.on("dragend", function(e) {
 				console.log(e);
 				localStorage.setItem(
 					"location-precise",
 					JSON.stringify(e.target._lngLat)
-				);
+					);
 			});
 		});
 
